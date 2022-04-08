ed4850b44ffa0e5c5ee0a4b7d24e794747b0a512
Boitumelo <Boitumelotshehla@gmail.com>
2022-04-08 12:19:05 -0700

    Description added

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,10 +9,15 @@
 /* betty style doc for function main goes there*/
 int main(void)
 {
+	
+/**
+ * Description:Program checks if the random number is less, greater or equal zero
+ */
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
@@ -25,5 +30,6 @@ int main(void)
 	{
 		printf("%d is negative\n", n);
 	}
+	
 	return (0);
 }
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,1 +1,2 @@
 0 The code accept a random number and print whether the number is negative or positive
+1 The code assign a random number to the variable n and print the last digit of the number stored in the variable

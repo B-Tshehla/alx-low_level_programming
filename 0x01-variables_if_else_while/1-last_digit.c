6b0dc700c5ad52cfeec4914c0faeea87ac9bbfb4
Boitumelo <Boitumelotshehla@gmail.com>
2022-04-08 13:14:28 -0700

    fixed correct output error

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -26,7 +26,7 @@ int main(void)
 	}
 	else if (n < 6)
 	{
-		printf("%s%d is %d and is less than 6 and not 0", str, n, last);
+		printf("%s%d is %d and is less than 6 and not 0\n", str, n, last);
 	}
 
 	return (0);

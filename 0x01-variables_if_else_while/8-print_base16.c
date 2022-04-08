d7bc1397632e3febfcf1f74f2f8feefa32f12ee2
Boitumelo <Boitumelotshehla@gmail.com>
2022-04-08 15:03:16 -0700

    8-print_base16.c created

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio>
+/**
+ * main-"Entry point"
+ * Return-"Always 0 (Success)"
+ */
+
+int main(void)
+{
+	char x;
+	
+	for (x = '0' ; x <= '9' ; x++)
+	{
+		putchar(x);
+	}
+	for (x = 'a' ; x <= 'f' ;x++)
+	{
+		putchar(x);
+	}
+		return (0);
+}

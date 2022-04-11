9b3b78c2bd57fef18bc291b184e17f7fd5037eee
Boitumelo <Boitumelotshehla@gmail.com>
2022-04-11 10:40:25 -0700

    101-print_comb4.c file

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,5 +6,27 @@
  */
 int main(void)
 {
+	int x;
+	int y;
+	int z;
+
+	for (x = 0; x < 8; x++)
+	{
+		for (y = x + 1; y < 9; y++)
+		{
+			for (z = y + 1; y < 10; z++)
+			{
+				putchar(x % 10) + '0';
+				putchar(y % 10) + '0';
+				putchar(z % 10) + '0';
+
+				if (x == 7 && y == 8 && z == 9)
+				{
+					continue;
+				}
+			}
+		}
+	}
+
 	return (0);
 }

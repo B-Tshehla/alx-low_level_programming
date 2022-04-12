c2db88cc7f2aadee65b989955dcc1edca5daa438
Boitumelo <Boitumelotshehla@gmail.com>
2022-04-12 12:22:00 -0700

    1-alphabet.c betty fix

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,4 @@
 #include "main.h"
-/**
- * main- "The function prints the alphabet in lowercase followed by a new line"
- *
- * Return: "Always 0 (Success)"
- */
 
 /**
  * print_alphabet - prints the alphabet in lowercase

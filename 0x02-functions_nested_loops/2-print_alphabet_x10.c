6e70e932f4c6f1c582bc3a8e7615c2a51ff8ccb5
Boitumelo <Boitumelotshehla@gmail.com>
2022-04-12 11:56:03 -0700

    2-print_alphabet_x10.c created

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,11 +2,12 @@
 /**
  * print_alphabet_x10 - function print 10 times the alphabet in lowercase
  */
-void print_alphabet_x10(void){
+void print_alphabet_x10(void)
+{
 	char let;
 	int x;
 
-	for (x = 0; x <= 10; x++)
+	for (x = 0; x < 10; x++)
 	{
 		for (let = 'a'; let <= 'z'; let++)
 		{

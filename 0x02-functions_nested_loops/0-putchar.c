73c42391a4fee18dac8dab804277846d4d559e7c
Boitumelo <Boitumelotshehla@gmail.com>
2022-04-12 10:06:32 -0700

    created main.h file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,20 @@
-#include <stdio.h>
+#include <main.h>
 /**
  * main-"The program prints _putchar followed by a new line"
- *
+ * File: "0-putchar.c"
  * Return- "Always 0 (Success)"
  */
 
 int main(void)
 {
-	printf("\_putchar");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	-putchar('r');
 
 	return (0);
 }
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * File: "main.h"
+ * Auth: "Boitumelo Tshehla"
+ * Desc: "The header file contains all declarations of functions used in the 0x02-functions_nested_loops directory."
+ */
+
+int _putchar(char c);
+
+#endif

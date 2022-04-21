a43b1d02606d0079d1125b9ba520883358966511
Boitumelo <Boitumelotshehla@gmail.com>
2022-04-21 04:35:05 -0700

    swap.c file created

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - "Swaps the values of two integers"
+ *
+ * @a : "Points to an int"
+ * @b : "Points to an int"
+ */
+void swap_int(int *a, int *b)
+{
+	int temp = *a;
+
+	*b = *a;
+	*a = tmp;
+}

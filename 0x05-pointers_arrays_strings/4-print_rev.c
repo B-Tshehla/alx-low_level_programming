44b5f4108f27b35230e09f942caa3a6aeea63bf6
Boitumelo <Boitumelotshehla@gmail.com>
2022-04-21 07:06:52 -0700

    print_rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_rev - "Prints a string, in reverse, followed by a new line"
+ *
+ * @s: "Points to a string that will be reverse"
+ */
+void print_rev(char *s)
+{
+		int len = 0;
+		int x;
+
+		while (s[x++])
+		{
+			len++;
+		}
+
+		for (x = len - 1; x >= 0; x--)
+		{
+			_putchar(s[x]);
+		}
+
+		_putchar('\n');
+}			

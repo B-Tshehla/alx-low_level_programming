fec72b1cd3d0798b36f6ba11e88562866cd84df5
Boitumelo <Boitumelotshehla@gmail.com>
2022-04-21 15:41:10 -0700

    put2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main"
+/**
+ * puts2 - "prints every other character of a string"
+ *
+ * @str: "string to be created"
+ */
+void puts2(char *str)
+{
+	int x;
+	int len = 0;
+
+	while (str[x] != '\0')
+	{
+		x++;
+	}
+
+	for (x = 0; x < len; x + 2)
+	{
+		_putchar(str[x]);
+	}
+
+	_putchar('\n');
+}

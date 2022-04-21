ee89903c8405d9d67685ea008ce07bb21ecd7794
Boitumelo <Boitumelotshehla@gmail.com>
2022-04-21 04:53:44 -0700

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *_strlen - Return the legnth of a string
+ *
+ *@s:"Points to a character"
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*str++)
+	{
+		len++
+	}
+	return (len);
+}

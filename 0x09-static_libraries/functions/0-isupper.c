8c9b47d7e8907b4380f050ef07d09e250cfe40f4
Boitumelo <Boitumelotshehla@gmail.com>
2022-05-03 08:13:38 -0700

    functions created

---
+++ 0x09-static_libraries/functions/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _isupper: "Chaeck for uppercase characters"
+ *
+ * @c: "character to be checked"
+ *
+ * Return: " an integer variable"
+ */
+int _isupper(int c)
+{
+	return (c >= 65 && c <= 90);
+}

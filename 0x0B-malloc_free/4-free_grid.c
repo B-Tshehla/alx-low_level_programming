b9e97188ac880970458cbcb6228d2b05ff7b0108
Boitumelo <Boitumelotshehla@gmail.com>
2022-05-04 10:53:25 -0700

    4-free_grid.c created

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+
+/**
+ * free_grid - "Free the grid"
+ * @grid: "grid of memories"
+ * @height: "integer"
+ * Return: "void"
+ */
+void free_grid(int **grid, int height)
+{
+	int x = 0;
+	
+	while (x < height)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}

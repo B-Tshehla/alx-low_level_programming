377b0f754049afb07b2d87cdb84f7cb536ccb06a
Boitumelo <Boitumelotshehla@gmail.com>
2022-05-06 02:41:52 -0700

    1-string_nconcat.c created

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,64 @@
+#include <stdlib>
+/**
+ * string_nconcat - "concatenates two strings"
+ * @s1: "first string"
+ * @s2: "second string"
+ * @n: "first bytes of string 2 to be used"
+ * Return:"pointer or NULL"
+ */
+
+unsigned int _strlen (char *s)
+{
+	unsigned int x = 0;
+
+	while (s[size] != '\0')
+	{
+		x++;
+	}
+	return (x);
+}
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int x = 0, y = 0;
+	char *s;
+
+	if (s1 == NULL)
+	{
+		s1 = "';
+	}
+	if (s2 == NULL)
+	{
+		s2  "";
+	}
+	
+	if (n < _strlen(s2))
+	{
+		m = malloc(_strlen(s1) + n * sizeof(char) + 1);
+	}
+	else
+	{
+		m = malloc(_strlen(s1) + _strlen(s2) + 2);
+	}
+	
+	if (m == 0)
+	{
+		return (NULL);
+	}
+
+	while (s1[x] != '\0')
+	{
+		s[x] = s1[x];
+		x++
+	}
+
+	while (s2[y] != '\0' && j < n)
+	{
+		s[x] = s2[y];
+		y++;
+		x++
+	}
+	s[x] = '\0';
+
+	return (s);
+}

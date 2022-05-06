4d4485c2638ca2bcb0bf25eae0681a1777225cbe
Boitumelo <Boitumelotshehla@gmail.com>
2022-05-06 02:59:09 -0700

    2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+/**
+ * _calloc - "allocates memory for an array, using malloc"
+ * @nmemb: "array length"
+ * @size: "size of each element"
+ * Return: "pointer"
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *ptr = s;
+	int x;
+
+	for (x = 0; x < n; x++)
+	{
+		*s++ = b;
+	}
+	return (ptr);
+}
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *s;
+
+	if (size == 0 || nmemb == 0)
+	{
+		return (NULL);
+	}
+	_memset(s, 0, nmemb * size);
+	
+	return (m);
+}

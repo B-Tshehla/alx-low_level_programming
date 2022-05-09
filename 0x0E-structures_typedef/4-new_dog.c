40350c29f859084bc527e5aef6c5daf1183158cf
Boitumelo <Boitumelotshehla@gmail.com>
2022-05-09 08:17:11 -0700

    submitted

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,6 +15,7 @@ int _strlen(char *s)
 	{
 		x++;
 	}
+	return (x);
 }
 /**
  * _strcpy - copies a pointed string
@@ -31,7 +32,7 @@ char *_strcpy(char *dest, char *src)
 		dest[x] = src[x];
 		x++;
 	}
-	dest[a] = '\0';
+	dest[x] = '\0';
 	return (dest);
 }
 /**

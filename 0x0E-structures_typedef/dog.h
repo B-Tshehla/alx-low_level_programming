edba841eb875f1656757d59fb1b2e713fdd2499d
Boitumelo <Boitumelotshehla@gmail.com>
2022-05-09 03:43:57 -0700

    dog.h file created

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - "structure definition of a dog"
+ * @name: "string"
+ * @age: "float"
+ * @owner:"character"
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *ownerl;
+} dog_t;

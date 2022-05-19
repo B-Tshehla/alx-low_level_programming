73dc46cbe125d469059ed7c42560a56464af5107
Boitumelo Tshehla <Boitumelotshehla@gmail.com>
2022-05-19 19:49:04 +0200

    Submit

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdlib.h>
+
+typedef struct list_t
+{
+	char *str;
+	unsigned int len;
+	struct list_t *next;
+};
 
 int _putchar(char c);
 size_t print_list(const list_t *h);

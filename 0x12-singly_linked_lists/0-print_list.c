1965d2d0d2f52f79b983d90f957441ffca35afb9
Boitumelo Tshehla <Boitumelotshehla@gmail.com>
2022-05-19 20:11:39 +0200

    Submit

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,10 +15,16 @@ size_t print_list(const list_t *h)
 
     while (currlist != NULL)
     {
-        printf("[%d] %s\n", currlist->len);
-        currlist->str != NULL ? currlist->str : "(nil)";
-        currlist = currlist->next;
-        numNodes++;
+        if (h->str == NULL)
+        {
+			printf("[0] (nil)\n");
+        }
+		else
+        {
+			printf("[%d] %s\n", h->len, h->str);
+        }
+		numNodes++;
+		h = h->next;
     }
     return (numNodes);
 }

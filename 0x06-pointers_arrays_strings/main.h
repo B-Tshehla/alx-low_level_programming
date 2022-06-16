d931ae3a19c176cffca8d4a342c6e54098a38ee1
B-Tshehla <Boitumelotshehla@gmail.com>
2022-06-16 17:35:57 +0200

    submit

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -11,4 +12,5 @@ char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
 void print_number(int n);
+int _putchar(char c);
 #endif
